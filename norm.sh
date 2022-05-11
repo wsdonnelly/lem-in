0405922ba331b4ed8e847c4d7680bd9b78a36a60
wsdonnelly <wsdonnelly@gmail.com>
2022-05-11 14:18:25 +0300

    final draft wed afternoon

---
+++ norm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+norminette sources
+norminette includes
+norminette maps/map_gen
+norminette visualizer/sources
+norminette visualizer/includes
+norminette libft/includes
+norminette libft/sources
