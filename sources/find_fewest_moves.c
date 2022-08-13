adbd9fd6b44b2dbf9746b81028d0031456a3ca50
Manuel Beeler <manuel.beeler90@gmail.com>
2022-08-13 20:21:25 +0300

    fixing the relinking of crossing paths -> delete rooms which appear multiple times on line

--- sources/find_fewest_moves.c
+++ sources/find_fewest_moves.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   find_fewest_moves.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wdonnell <wdonnell@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manuelbeeler <manuelbeeler@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 12:49:06 by jjuntune          #+#    #+#             */
-/*   Updated: 2022/06/03 12:13:49 by wdonnell         ###   ########.fr       */
+/*   Updated: 2022/08/13 20:04:03 by manuelbeele      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
--- sources/map_paths.c
+++ sources/map_paths.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_paths.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wdonnell <wdonnell@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manuelbeeler <manuelbeeler@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 12:49:06 by jjuntune          #+#    #+#             */
-/*   Updated: 2022/05/23 10:27:08 by wdonnell         ###   ########.fr       */
+/*   Updated: 2022/08/13 20:15:46 by manuelbeele      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,6 @@ static void	relink_paths(t_path *shortest_path, t_path *path, int *switched)
 	path->next_room = tmp1->next_room;
 	free(tmp1);
 	free(tmp2);
-	tmp1 = path;
-	path = shortest_path;
-	shortest_path = tmp1;
 	*switched = 1;
 }
 
@@ -40,7 +37,43 @@ static void	iterate_paths(t_paths *paths, int *switched, char **rooms, \
 		if (!ft_strcmp(path->room->name, rooms[1])
 			&& !ft_strcmp(path->next_room->room->name, rooms[0]))
 			relink_paths(shortest_path, path, switched);
-		path = path->next_room;
+		if (!*switched)
+			path = path->next_room;
+	}
+}
+
+void	delete_rooms(t_path *shortest_path)
+{
+	t_path	*tmp;
+
+	tmp = shortest_path->next_room;
+	shortest_path->next_room = tmp->next_room;
+	free(tmp);
+}
+
+void	check_new_path(t_path *shortest_path)
+{
+	t_path	*tmp;
+	int		switched;
+
+	while (shortest_path)
+	{
+		switched = 0;
+		tmp = shortest_path->next_room;
+		while (tmp && !switched)
+		{
+			if (!ft_strcmp(tmp->room->name, shortest_path->room->name))
+			{
+				while (ft_strcmp(shortest_path->room->name, \
+						shortest_path->next_room->room->name))
+					delete_rooms(shortest_path);
+				delete_rooms(shortest_path);
+				switched = 1;
+			}
+			if (!switched)
+				tmp = tmp->next_room;
+		}
+		shortest_path = shortest_path->next_room;
 	}
 }
 
@@ -58,10 +91,16 @@ void	map_paths(t_paths *all_paths, t_path *shortest_path)
 		paths = all_paths;
 		while (paths && !switched)
 		{
-			iterate_paths(all_paths, &switched, rooms, shortest_path);
-			paths = paths->next_path;
+			iterate_paths(paths, &switched, rooms, shortest_path);
+			if (!switched)
+				paths = paths->next_path;
 		}
 		if (!switched)
 			shortest_path = shortest_path->next_room;
+		else
+		{
+			shortest_path = paths->path;
+			check_new_path(shortest_path);
+		}
 	}
 }
