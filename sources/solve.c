c4d10058ebc4735a8858702193cd74fcc665a126
William Donnelly <wdonnell@c1r3p2.hive.fi>
2022-10-07 15:23:34 +0300

    seems to be working

--- sources/solve.c
+++ sources/solve.c
@@ -6,7 +6,7 @@
 /*   By: wdonnell <wdonnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 13:27:44 by wdonnell          #+#    #+#             */
-/*   Updated: 2022/10/07 14:15:22 by wdonnell         ###   ########.fr       */
+/*   Updated: 2022/10/07 15:22:28 by wdonnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static void	print_path_test(t_data *data, t_room *graph)
 	}
 	printf("\n");
 }
-/*
+
 static void	init_in_path(t_data *data, t_room *graph)
 {
 	int	i;
@@ -65,7 +65,7 @@ static void	filter_all_rooms(t_data *data, t_room *graph)
 		i++;
 	}
 }
-*/
+
 /*
 * change_capacity(data, graph, SAVE?, FLOW?)
 * bfs(data, graph, PATH?)
@@ -77,21 +77,21 @@ void	solve(t_data *data, t_room *graph)
 
 	data->path_group = NULL;
 	//data->path_set = NULL;
-	//filter_all_rooms(data, graph);
+	filter_all_rooms(data, graph);
 	while (data->flow_path)
 	{
 		bfs(data, graph, FALSE);
 		if (data->flow_path)
 		{
-			printf("found a flow\n");
-			//change_capacity(data, graph, FALSE, TRUE);
+			//printf("found a flow\n");
+			change_capacity(data, graph, FALSE, TRUE);
 			
 			create_path_group(data);
-			steps = change_capacity(data, graph, TRUE, TRUE);
-			print_graph_test(graph, data);
-			create_path_set(data, data->cur_path, steps);
-			//init_in_path(data, graph);
-			/*
+			//steps = change_capacity(data, graph, TRUE, TRUE);
+			//print_graph_test(graph, data);
+			//create_path_set(data, data->cur_path, steps);
+			init_in_path(data, graph);
+			
 			while (data->shortest_path)
 			{
 				bfs(data, graph, TRUE);
@@ -101,7 +101,7 @@ void	solve(t_data *data, t_room *graph)
 					create_path_set(data, data->cur_path, steps);
 				}
 			}
-			*/
+			
 		}
 	}
 	print_path_test(data, graph);
